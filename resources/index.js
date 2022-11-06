14d434ae1205b9d9dec04b03172713876e167ee1
Dent24 <65475471+Dent24@users.noreply.github.com>
2022-11-06 14:46:12 +0800

    Create vue-router

--- resources/index.js
+++ resources/index.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
+import { createWebHistory, createRouter } from "vue-router"
 import main from './component/main.vue'
+import Manage from './component/manage/main.vue'
+import Customer from './component/customer/main.vue'
 
-createApp(main).mount('#app')
+const routes = [
+    {
+        path: "/manage",
+        name: "Manage",
+        component: Manage
+    },
+    {
+        path: "/customer",
+        name: "Customer",
+        component: Customer
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+createApp(main).use(router).mount('#app')
